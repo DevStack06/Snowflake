9e9aa44ed3852fa2128fd689b9c30b9acf8733a1
Balram Rathore <balram0698@gmail.com>
2021-04-29 12:39:00 +0530

    new rows added

--- 2nd.sql
+++ 2nd.sql
@@ -1,3 +1,3 @@
 
-INSERT INTO emp_basic (first_name, last_name, email,city)
-VALUES ("Balram", "Rathore", "test@12344","Bilaspur"); 
+INSERT INTO emp_basic (first_name, last_name,  email,streetaddress,city)
+VALUES ("Balram", "Rathore", "test@12344","Santoshipara","Bilaspur"); 
