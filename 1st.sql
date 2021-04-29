b2d55f5a774178d78cce5ffcf055ef0e05ba0830
Balram Rathore <balram0698@gmail.com>
2021-04-29 12:32:15 +0530

    new rows added

--- 1st.sql
+++ 1st.sql
@@ -5,10 +5,7 @@ CREATE OR REPLACE TABLE emp_basic (
   streetaddress STRING ,
   city STRING ,
   );
-INSERT [ OVERWRITE ] INTO emp_basic [ (  [first_name, last_name, email,city] ) ]
-                                         { { VALUES ( { "Balram", "Rathore", "test@12344","Bilaspur"}) }}
-INSERT [ OVERWRITE ] INTO emp_basic [ (  [first_name, last_name, email,city] ) ]
-                                         { { VALUES ( { "Sourav", "Mallick", "123@test","Kolkata"} )}}
+
 -- INSERT INTO emp_basic (first_name, last_name, email,city)
 -- VALUES ("Balram", "Rathore", "test@12344","Bilaspur"); 
 -- INSERT INTO emp_basic (first_name, last_name, email,city)
