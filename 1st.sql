1bb2b6d7e424dda269ef975ef42a7b42098ebaff
Balram Rathore <balram0698@gmail.com>
2021-04-29 12:34:32 +0530

    new rows added

--- 1st.sql
+++ 1st.sql
@@ -6,7 +6,3 @@ CREATE OR REPLACE TABLE emp_basic (
   city STRING ,
   );
 
--- INSERT INTO emp_basic (first_name, last_name, email,city)
--- VALUES ("Balram", "Rathore", "test@12344","Bilaspur"); 
--- INSERT INTO emp_basic (first_name, last_name, email,city)
--- VALUES ("Sourav", "Mallick", "123@test","Kolkata"); 
