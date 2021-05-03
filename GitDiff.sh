7fd00c72c8c5b3143be9613deeb70a4ca273dfc1
Balram Rathore <balram0698@gmail.com>
2021-05-03 12:45:24 +0530

    new changes

--- GitDiff.sh
+++
@@ -1,9 +0,0 @@
-git diff-tree -r   master developer --name-only --diff-filter=ACMRT  > gitdiff.txt
-
-
-#!/bin/bash
-input="gitdiff.txt"
-while IFS= read -r line
-do
-  echo "$line"
-done < "$input"
--- refer.sql
+++
@@ -1,6 +0,0 @@
--- INSERT [ OVERWRITE ] INTO emp_basic [ (  [first_name, last_name, email,city] ) ]
---                                          { { VALUES ( { "Balram", "Rathore", "test@12344","Bilaspur"}) }}
--- INSERT [ OVERWRITE ] INTO emp_basic [ (  [first_name, last_name, email,city] ) ]
---                                          { { VALUES ( { "Sourav", "Mallick", "123@test","Kolkata"} )}}
--- INSERT INTO emp_basic (first_name, last_name, email,city)
--- VALUES ("Sourav", "Mallick", "123@test","Kolkata"); 
