f34953f89ffdc96a01293b27c407c653a0ec676e
Balram Rathore <balram0698@gmail.com>
2021-05-06 13:10:48 +0530

    new changes

--- InsertRow.sql
+++ InsertRow.sql
@@ -1,5 +1,5 @@
 
 INSERT INTO emp_basic2 (first_name, last_name,  email,streetaddress,city)
-VALUES ('Balram1', 'Rathore', 'est@12344','Santoshipara','Bilaspur');
+VALUES ('Balram1', 'Rathore1', 'est@12344','Santoshipara','Bilaspur');
 -- INSERT INTO emp_basic (first_name, last_name,  email,streetaddress,city)
 -- VALUES ('Dev', 'Stack', 'est@12344','Santoshipara','Bilaspur'); 
